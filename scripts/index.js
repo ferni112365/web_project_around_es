1c931d8f900dada03e962db33eeec1d812708aaa
Fernando Hernandez <ferni112365@gnail.com>
2026-04-24 00:18:25 -0600

    Feat: agregar oppenModal de nueva tarjeta

--- scripts/index.js
+++ scripts/index.js
@@ -114,5 +114,8 @@ initialCards.forEach((card) => {
 });
 
 const openModalCard = document.querySelector(".profile__add-button");
-const newCardForm = document.querySelector("#new-card-form");
+const newCardPopup = document.querySelector("#new-card-popup");
 const closeButtonCard = document.querySelector(".popup__close");
+openModalCard.addEventListener("click", function () {
+  openModal(newCardPopup);
+});
