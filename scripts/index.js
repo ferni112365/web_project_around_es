af2e9519e8155dd7c80fb662f1250c26a191bb8e
Fernando Hernandez <ferni112365@gnail.com>
2026-06-25 00:55:41 -0600

    feat: agregar getTemplate

--- scripts/index.js
+++ scripts/index.js
@@ -304,4 +304,12 @@ class Card {
     this._link = data.link;
     this._cardSelector = cardSelector;
   }
+
+  _getTemplate() {
+    const cardElement = document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
+    return cardElement;
+  }
 }
