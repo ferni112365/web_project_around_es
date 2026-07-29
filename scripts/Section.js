cca77f88b2e50e5092add749a1158d4411d67c36
Fernando Hernandez <ferni112365@gnail.com>
2026-07-28 23:48:13 -0600

    Feat: agregar class Section y constructor

--- scripts/Section.js
+++ scripts/Section.js
@@ -1,1 +1,15 @@
+class Section {
+  #items;
+  #renderer;
+  #container;
 
+  constructor({ items, renderer }, containerSelector) {
+    this.#items = items;
+    this.#renderer = renderer;
+    this.#container = document.querySelector(containerSelector);
+  }
+
+  renderer()
+
+  addItem()
+}
