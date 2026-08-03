4c80c821d5d34cf17a77ae8fedbc89fde9739aba
Fernando Hernandez <ferni112365@gnail.com>
2026-08-03 01:01:51 -0600

    Refactor: agregar el selector del overlay (popupContent)

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -2,20 +2,22 @@ class Popup {
   #popupSelector;
   #handlePopupClose;
   #closeButton;
+  #popupContent;
 
   constructor(popupSelector) {
     this.#popupSelector = document.querySelector(popupSelector);
     this.#handlePopupClose = this.#handleEscClose.bind(this);
     this.#closeButton = this.#popupSelector.querySelector(".popup__close");
+    this.#popupContent = this.#popupSelector.querySelector(".popup__content");
   }
 
   open() {
     this.#popupSelector.classList.add("popup_is-opened");
     document.addEventListener("keydown", this.#handlePopupClose);
-    this.popupSelector.addEventListener(
-      "click",
-      this.setEventListeners.bind(this),
-    );
+    // this.popupSelector.addEventListener(
+    //   "click",
+    //   this.setEventListeners.bind(this),
+    // );
   }
 
   close() {
@@ -36,6 +38,12 @@ class Popup {
   setEventListeners() {
     this.#closeButton.addEventListener("click", (evt) => {
       this.close();
+    });
+
+    this.#popupContent.addEventListener("click", (evt) => {
+      if (evt.target === evt.currentTarget) {
+        this.close();
+      }
     });
   }
 }
