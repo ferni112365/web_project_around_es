032432f21f65a429cb999bd60c63c4648f8a48c5
Fernando Hernandez <ferni112365@gnail.com>
2026-08-04 23:04:20 -0600

    Feat: agregar constructor de class PopupWithForm

--- scripts/PopupWithForm.js
+++ scripts/PopupWithForm.js
@@ -1,3 +1,8 @@
 import { Popup } from "./Popup";
 
-export class PopupWithForm extends Popup {}
+export class PopupWithForm extends Popup {
+  constructor(popupSelector, handleFormSubmit) {
+    super(popupSelector);
+    this.handleFormSubmit = handleFormSubmit;
+  }
+}
