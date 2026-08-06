f2d8b92a861bc5659a0daea73188c9fd904a439f
Fernando Hernandez <ferni112365@gnail.com>
2026-08-05 21:13:15 -0600

    Feat: agregar método #getInputValues de clase PopupWithForm

--- scripts/PopupWithForm.js
+++ scripts/PopupWithForm.js
@@ -4,5 +4,15 @@ export class PopupWithForm extends Popup {
   constructor(popupSelector, handleFormSubmit) {
     super(popupSelector);
     this.handleFormSubmit = handleFormSubmit;
+    this.forElement = this.#popupSelector.querySelector("popup__form");
+    this.inputList = this.formElement.querySelectorAll(".popup__input");
+  }
+
+  #getInputValues() {
+    const formValues = {};
+    this.inputList.forEach((input) => {
+      formValues[input.name] = input.value;
+    });
+    return formValues;
   }
 }
