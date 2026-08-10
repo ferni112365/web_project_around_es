c5bc0c1da767dfcf3884e5747441dab23878d2f3
Fernando Hernandez <ferni112365@gnail.com>
2026-08-09 22:32:06 -0600

    Refactor: eliminar código comentado de la clase PopupWithImage

--- scripts/PopupWithImage.js
+++ scripts/PopupWithImage.js
@@ -1,15 +1,11 @@
 class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
-    // aquí seleccionas los elementos internos del popup que necesitas,
-    // por ejemplo la imagen y el texto de la descripción
     this.image = this.popupElement.querySelector(".popup__image");
     this.description = this.popupElement.querySelector(".popup__caption");
   }
 
   open(name, link) {
-    // aquí defines qué hace este popup en particular al abrirse,
-    // por ejemplo asignar el src y alt de la imagen
     this.image.src = link;
     this.image.alt = name;
     this.description.textContent = name;
