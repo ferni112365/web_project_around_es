3c4a66ed29d4fbbd2f4d693cfce588712b6c3f76
Fernando Hernandez <ferni112365@gnail.com>
2026-08-11 00:33:41 -0600

    Refactor: eliminar código comentado de index.js

--- scripts/index.js
+++ scripts/index.js
@@ -37,21 +37,12 @@ const editProfileModal = document.querySelector("#edit-popup");
 const profileCloseBtn = editProfileModal.querySelector(".popup__close");
 const cardTemplate = document.querySelector("#card-template");
 
-// const popUpImage = document.querySelector("#image-popup");
-// const modalImage = popUpImage.querySelector(".popup__image");
-// const modalCaption = popUpImage.querySelector(".popup__caption");
-// const modalCloseBtn = popUpImage.querySelector(".popup__close");
-
 profileEditBtn.addEventListener("click", function () {
   handleOpenEditModal();
 });
 
 profileCloseBtn.addEventListener("click", function () {
   closeModal(editProfileModal);
-});
-
-modalCloseBtn.addEventListener("click", function () {
-  closeModal(popUpImage);
 });
 
 function fillProfileForm() {
