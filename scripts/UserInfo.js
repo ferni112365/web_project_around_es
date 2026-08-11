e9f155b92ebe5a108bb07462920c9b9473c154fb
Fernando Hernandez <ferni112365@gnail.com>
2026-08-11 00:36:29 -0600

    Feat: agregar sintaxis básica de la clase UserInfo

---
+++ scripts/UserInfo.js
@@ -0,0 +1,9 @@
+class UserInfo {
+  constructor({ nameSelector, jobSelector }) {
+    // aquí guardas los elementos del DOM usando document.querySelector()
+  }
+
+  getUserInfo() {}
+
+  setUserInfo() {}
+}
